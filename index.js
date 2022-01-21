dbba55d0f8c9176f0050a34f26b562c7e6db6112
Victor Brambila <victorbrambila22@gmail.com>
2022-01-21 12:05:32 -0800

    topic 2 request 1

--- index.js
+++ index.js
@@ -63,9 +63,13 @@ const zooAnimals = [
   displayNames will be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(/*Your Code Here*/){
-    /*Your Code Here*/
+  function animalNames(arr){
+    const displayNames = [];
+    arr.forEach( item => displayNames.push(`name: ${item.animal_name}, scientific: ${item.scientific_name}`));
+    return displayNames;
   }
+
+  //console.log('task 3', animalNames(zooAnimals));
   
 
   /* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
